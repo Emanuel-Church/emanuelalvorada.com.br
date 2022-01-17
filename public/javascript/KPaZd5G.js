602f54b91d1839dbbdf8dc65c0227a2fb303439f
Henry Rodrigues <85399516+hhenry-rodrigues@users.noreply.github.com>
2022-01-17 13:11:40 -0300

    Add files via upload

--- public/javascript/KPaZd5G.js
+++ public/javascript/KPaZd5G.js
@@ -6,3 +6,9 @@ async function clipboardCopy() {
   let text = document.querySelector("#__ime-pix").value;
   await navigator.clipboard.writeText(text);
 }
+
+$(document).ready(function(){
+  $("#clipboardCopy").on("click", function(){
+      $("#pixalert").dialog();
+  });
+});
